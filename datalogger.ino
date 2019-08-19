04affc41f2c0a32f6d8d26d0493f5d7e7f0020b8
jake waters <jerkey@g-cipher.net>
2019-08-19 02:37:35 -0700

    modify program for scooter spying

--- datalogger.ino
+++ datalogger.ino
@@ -1,23 +1,9 @@
-/*
-  SD card datalogger
-
- This example shows how to log data from three analog sensors
- to an SD card using the SD library.
-
- The circuit:
- * analog sensors on analog ins 0, 1, and 2
+/* SD card datalogger
  * SD card attached to SPI bus as follows:
  ** MOSI - pin 11
  ** MISO - pin 12
  ** CLK - pin 13
  ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
-
- created  24 Nov 2010
- modified 9 Apr 2012
- by Tom Igoe
-
- This example code is in the public domain.
-
  */
 
 #include <SPI.h>
@@ -25,35 +11,38 @@
 
 const int chipSelect = 4;
 
-void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+uint32_t lastBMSPacket = 0; // when was the last time we got a BMS data
+int8_t packetProgress = 0; // how far along in a decoded packet are we
+uint8_t packetLength = 0; // received packet length
+uint16_t remainingcapacity; //offset 0x62-0x63
+uint16_t remainingpercent; //offset 0x64-0x65
+int16_t current; //offset 0x66-67 - negative = charging; /100 = Ampere
+uint16_t voltage; //offset 0x68-69 /10 = Volt
+uint8_t temperature[2]; //offset 0x6A-0x6B -20 = °C
 
+
+void setup() {
+  Serial.begin(115200); //while (!Serial); // wait for serial port to connect. Needed for native USB port only
 
   Serial.print("Initializing SD card...");
 
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
+  if (!SD.begin(chipSelect)) { // see if the card is present and can be initialized:
     Serial.println("Card failed, or not present");
-    // don't do anything more:
-    while (1);
+    //while (1); // don't do anything more:
+  } else {
+    Serial.println("card initialized.");
   }
-  Serial.println("card initialized.");
 }
 
 void loop() {
   // make a string for assembling the data to log:
   String dataString = "";
 
-  // read three sensors and append to the string:
   for (int analogPin = 0; analogPin < 3; analogPin++) {
     int sensor = analogRead(analogPin);
-    dataString += String(sensor);
+    logString += String(sensor);
     if (analogPin < 2) {
-      dataString += ",";
+      logString += ",";
     }
   }
 
@@ -63,10 +52,10 @@ void loop() {
 
   // if the file is available, write to it:
   if (dataFile) {
-    dataFile.println(dataString);
+    dataFile.println(logString);
     dataFile.close();
     // print to the serial port too:
-    Serial.println(dataString);
+    Serial.println(logString);
   }
   // if the file isn't open, pop up an error:
   else {
