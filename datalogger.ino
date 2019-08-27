b48f47596eaee1cf5eb64d292ac9f6e6b1346440
jake waters <jerkey@g-cipher.net>
2019-08-27 01:18:42 -0700

    chipSelect pin 10

--- datalogger.ino
+++ datalogger.ino
@@ -23,11 +23,11 @@
 #include <SPI.h>
 #include <SD.h>
 
-const int chipSelect = 4;
+const int chipSelect = 10;
 
 void setup() {
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
@@ -72,13 +72,5 @@ void loop() {
   else {
     Serial.println("error opening datalog.txt");
   }
+  delay(500); // let's not go too fast
 }
-
-
-
-
-
-
-
-
-
